6c0c3a9ebaa3e42ac36a635921b52d99035f5d30
azeroth29 <azeroth297@gmail.com>
2022-09-06 00:00:25 +0700

    remove secret
    remove secret

--- Exercise_2/variables.tf
+++ Exercise_2/variables.tf
@@ -1,13 +1,10 @@
 # TODO: Define the variable for aws_region
 variable "access_key" {
-  default = "ASIASC3MIA7ET4EISAMY"
 }
 variable "secret_key" {
-  default = "3N+WlvXyBd2T/C1Gmxyj1aQThC4ye7DGrEoIytFl"
 }
 
 variable "token" {
-  default = "FwoGZXIvYXdzEGIaDAthEr1LDo49bc/1PCLVAQ/ongy/RqBuNYKEfs+YeiPcCO9kVa+5riJg07osYsXNvpRbn+L8KtCZcgcBUNA8FZNZfwtImnAJuSCdlhiV1Zy8mxxllekxSGJDebewRgtna4FwAYR+qW2NJhDiosHQrU9qCItXxp/yduw1XDYIZOt3o4IjCb7+6JWupfjG3hiHFwqKpTaYoJEMjIsLOA8yarEIZ4IfmVE50X1IdtN7MGcEK3xQEUztfLgMwzw+RPI/R+GJe73XhGkLQnqbeVWNdFFSv1lcJwp2/IirRwzSoco8+V2RPCjaxdiYBjItVIBdfzwMkvOUR1xmK0ynWl/rDM4UgtfA2IekXTPq69s0t00Okn8ENIl7fXir"
 }
 variable "region" {
   default = "us-east-1"
